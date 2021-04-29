83175e476a48775857abb707b1a61533995b246e
arlenediannel <arlenediannel@gmail.com>
2021-04-29 23:35:13 +0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Component } from 'react';
 import { CardList } from './components/card-list/card-list.component';
 import { SearchBox } from './components/search-box/search-box.components.jsx';
 
+//aaa
 class App extends Component{
   constructor() {
     super();
